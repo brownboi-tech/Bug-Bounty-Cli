85004b98d2eabbbc53d1654ae579c69687508ee1
Dmwsec <brownboi@protonmail.com>
2026-05-12 08:37:48 +0530

    Create enrichment.py

---
+++ enrichment.py
@@ -0,0 +1,19 @@
+%%writefile /content/enrichment.py
+
+import json
+
+def enrich_findings(vulnerabilities):
+    print("[Enrichment] Mapping findings to CWE database...")
+    try:
+        with open('/content/cwe_map.json', 'r') as f:
+            cwe_data = json.load(f)
+    except FileNotFoundError:
+        cwe_data = {}
+
+    for bug in vulnerabilities:
+        bug_type = bug.get("type")
+        if bug_type in cwe_data:
+            bug["cwe_id"] = cwe_data[bug_type].get("id")
+            bug["description"] = cwe_data[bug_type].get("desc")
+    
+    return vulnerabilities
